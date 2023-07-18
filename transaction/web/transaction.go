9656eae347f73322c7012a0f2acb7c47ac2be13d
Vishal Sharma <91780492+Vissharm19@users.noreply.github.com>
2023-07-18 13:54:15 +0100

    intital refactor

--- transaction/web/transaction.go
+++ transaction/web/transaction.go
@@ -3,18 +3,13 @@ package web
 import (
 	"fmt"
 	"net/http"
-
-	"shl/bnk/cc/dao"
-	"shl/bnk/cc/model"
-
+	"dao"
 	"github.com/labstack/echo/v4"
 )
 
 func Accounts(c echo.Context) error {
-	trans := model.PendingTransResponse{}
-	arrangmentId := c.Param("arrangmentId")
-	trans.Cid = arrangmentId
-	trans.Comment = "PostTransaction"
+
+	db := dao.setupDB()
 
 	return c.JSON(http.StatusOK, trans)
 }
@@ -48,5 +43,3 @@ func TransactionSearch(c echo.Context) error {
 	trans.Comment = string(dpResponse.Body())
 	return c.JSON(http.StatusOK, trans)
 }
-
-
